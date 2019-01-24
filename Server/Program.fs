8b33a24e6173f027c28c447d692b45d53738d02a
Björn Kihlberg <bjorn.wilhelm.kihlberg@gmail.com>
2019-01-24 17:55:30 +0100

    serve static files in public folder

--- Server/Program.fs
+++ Server/Program.fs
@@ -1,4 +1,30 @@
-﻿[<EntryPoint>]
+﻿open System.IO
+open Microsoft.AspNetCore.Builder
+open Microsoft.AspNetCore.Hosting
+open Microsoft.Extensions.DependencyInjection
+open Giraffe
+
+let endpoints = choose []
+
+type Startup() =
+    member __.ConfigureServices (services: IServiceCollection): unit =
+        services.AddGiraffe() |> ignore
+    
+    member __.Configure (app: IApplicationBuilder): unit =
+        app
+            .UseDefaultFiles()
+            .UseStaticFiles()
+            .UseGiraffe endpoints
+
+[<EntryPoint>]
 let main _ =
-    printfn "Hello, world!"
+    let publicPath = Path.GetFullPath "./public"
+
+    WebHostBuilder()
+        .UseKestrel()
+        .UseWebRoot(publicPath)
+        .UseContentRoot(publicPath)
+        .UseStartup<Startup>()
+        .Build()
+        .Run()
     0 // return an integer exit code
