e06a464782e1bcdfee53f68758dccb3f2218d5bf
Björn Kihlberg <bjorn.wilhelm.kihlberg@gmail.com>
2019-01-26 13:11:58 +0100

    enable signalr

--- Server/Program.fs
+++ Server/Program.fs
@@ -1,15 +1,23 @@
 ﻿open System
 open System.IO
+open Microsoft.AspNetCore.Http
 open Microsoft.AspNetCore.Builder
 open Microsoft.AspNetCore.Hosting
+open Microsoft.AspNetCore.SignalR
 open Microsoft.Extensions.DependencyInjection
 open Giraffe
 
 let endpoints = choose []
 
+type MyHub() =
+    inherit Hub()
+    member __.Receive(msg: string) =
+        printfn "msg received: %s" msg
+
 let configureApp (app: IApplicationBuilder): unit =
     app.UseDefaultFiles()
        .UseStaticFiles()
+       .UseSignalR(fun routes -> routes.MapHub<MyHub>(PathString "/myhub"))
        .UseGiraffe endpoints
 
 let configureServices (services: IServiceCollection): unit =
